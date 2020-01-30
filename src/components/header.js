405b86543912c30bf5e61d0bb92c2e4219499f44
Kenneth Rafferty <krafferty71@gmail.com>
2020-01-30 14:07:23 +0000

    removal of images and blue background

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background:"#005EB8",
       marginBottom: `1.45rem`,
     }}
   >
